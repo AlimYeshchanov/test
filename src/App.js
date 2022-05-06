12c613569164a60a2771fc86ba756336f1f75adb
Alim <alim_yaf@mail.ru>
2022-05-06 10:11:23 +0600

    entered commit in App.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from "react-bootstrap"
 
-
+///affsdfsdfsfsfsfsdfsdf
 
 const ThemeContext = React.createContext('light');
 
